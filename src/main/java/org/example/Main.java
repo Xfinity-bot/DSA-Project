0d50a994bf3b981c7ec7e6c870725a3eb1fa313c
Sriganesh Rao <2022sp93025@wilp.bits-pilani.ac.in>
2023-05-02 10:48:41 +0530

    Bug fix

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void main(String[] args){
+        PatientRecord patientRecord = new PatientRecord();
+
+        // Add some patient records
+        int id1 = patientRecord.addPatient("Alice", 70);
+        int id2 = patientRecord.addPatient("Bob", 80);
+        int id3 = patientRecord.addPatient("Charlie", 75);
+
+        // Display all patient records
+        patientRecord.displayPatients();
+
+        // Delete a patient record
+        patientRecord.deletePatient(id2);
+
+        // Display all patient records again
+        patientRecord.displayPatients();
     }
+
 }
